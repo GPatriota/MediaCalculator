671e94a7fddd66ac2a0816fb837b2d3e5721d155
GPatriota <gpspatriota@gmail.com>
2025-01-17 13:08:06 -0300

    Challenge Finished

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
+let numberGrades = 2;
 document.getElementById("calculate").addEventListener("click", function() {
     const name = document.getElementById("name").value;
-    const grade1 = parseFloat(document.getElementById("grade1").value);
-    const grade2 = parseFloat(document.getElementById("grade2").value);
-    const grade3 = parseFloat(document.getElementById("grade3").value);
-  
+    const grades = document.querySelectorAll('#grades .grade');
     const resultDiv = document.getElementById("result");
-  
-    
-    if (!name || isNaN(grade1) || isNaN(grade2) || isNaN(grade3)) {
-      resultDiv.innerHTML = `<p style="color: red;">Preencha todos os campos corretamente.</p>`;
+    let verifyInputs = false;
+
+    function calculateMedia(grades){
+      let sumGrades = 0;
+      grades.forEach(grade => {
+        const gradeValue = parseFloat(grade.value);
+        if(isNaN(gradeValue) || gradeValue < 0 || gradeValue > 10 || gradeValue === ''){
+          resultDiv.innerHTML = `<p style="color: red;">Preencha todos os campos corretamente.</p>`;
+          verifyInputs = true;
+          return;
+        }
+        sumGrades += gradeValue;
+      });
+      return sumGrades / grades.length;
+    }
+
+    const media = calculateMedia(grades);
+
+    if (verifyInputs){
       return;
     }
-  
-    
-    function calculateMedia (n1,n2,n3){
-        const media = (n1 +n2 + n3) / 3;
-        return media;
-    }
-    // const calculateMedia = (n1, n2, n3) => (n1 + n2 + n3) / 3;
-  
-    const media = calculateMedia(grade1, grade2, grade3);
-  
-    
     const determineStatus = (media) => {
       if (media >= 7) return "Aprovado";
       if (media >= 5) return "Recuperação";
@@ -37,4 +39,17 @@ document.getElementById("calculate").addEventListener("click", function() {
       <p><strong>Status:</strong> ${status}</p>
     `;
   });
+
+
+
+  document.getElementById('addGrade').addEventListener('click', function() {
+    const div = document.getElementById('grades');
+    numberGrades += 1;
+
+    const htmlForm = `
+    <label for="grade${numberGrades}">Nota ${numberGrades}:</label>
+    <input type="number" id="grade${numberGrades}" class="grade"placeholder="Digite a ${numberGrades}ª nota" min="0" max="10" required>`;
+
+    div.innerHTML += htmlForm;
+  })
   
